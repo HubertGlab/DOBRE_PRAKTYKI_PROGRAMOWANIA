c70825eb4ab613705d593ad1f81ed806fd46d2c1
Hubercik :) <112071381+HubertGlab@users.noreply.github.com>
2024-11-24 18:35:03 +0100

    Adding bold and color to print

--- classes_test.py
+++ classes_test.py
@@ -8,9 +8,11 @@ catalog1 = LibraryCatalog()
 catalog2 = LibraryCatalog()
 catalog1.add_book("Book 1")
 catalog2.add_book("Book 2")
+print('\033[95mSingleton:\033[0;0m')
 print(catalog1.get_books())
 
 """adapter test"""
+print('\033[95mAdapter:\033[0;0m')
 json_data = '''
 [
     {"title": "Book 1"},
@@ -43,15 +45,17 @@ xml_data = '''
 print("XML Result:", BookDataAdapter.adapt_from_xml(xml_data))
 
 """Factory test"""
-student = UserFactory.create_user("Student")
-teacher = UserFactory.create_user("Teacher")
-librarian = UserFactory.create_user("Librarian")
+print('\033[95mFactory:\033[0;0m')
+student = UserFactory.create_user("Student", 'std')
+teacher = UserFactory.create_user("Teacher", 'tch')
+librarian = UserFactory.create_user("Librarian", 'lib')
 
 print('student permission ', student.get_permissions())
 print('teacher permission ', teacher.get_permissions())
 print('librarian permission ', librarian.get_permissions())
 
 """Observer test"""
+print('\033[95mObserver:\033[0;0m')
 student = UserFactory.create_user("Student", "Alice")
 catalog1 = LibraryCatalog()
 catalog1.subscribe(student)
